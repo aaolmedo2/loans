e12538510fdc6ee593e51614db4cf4c599acd5ab
Angelo Andres Olmedo Camacho <aaolmedo2@espe.edu.ec>
2025-06-21 19:21:59 -0500

    Initial commit

---
+++ src/main/java/com/banquito/core/loans/LoansModuleApplication.java
@@ -0,0 +1,13 @@
+package com.banquito.core.loans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansModuleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banquito/core/loans/LoansModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banquito.core.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
